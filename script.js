c3ade89c7de3397159fc8b111d4e29e485a37471
Bohdan Husiev <bgusev77@gmail.com>
2023-06-27 09:28:42 +0300

    add-static

--- script.js
+++ script.js
@@ -5,45 +5,50 @@ class Hamburger {
     this.toppings = [];
   }
 
-  addTopping(...topping) {
-    this.toppings.push(...topping);
+  static sizes = {
+    small: { price: 50, calories: 20 },
+    large: { price: 100, calories: 40 }
+  };
+
+  static stuffings = {
+    cheese: { price: 10, calories: 20 },
+    salad: { price: 20, calories: 5 },
+    potato: { price: 15, calories: 10 }
+  };
+
+  static toppings = {
+    spice: { price: 15, calories: 0 },
+    mayo: { price: 20, calories: 5 }
+  };
+
+  addTopping(...toppings) {
+    this.toppings.push(...toppings);
   }
 
   calculatePrice() {
-    let totalPrice = this.size.price + this.stuffing.price;
+    let totalPrice =
+      Hamburger.sizes[this.size].price +
+      Hamburger.stuffings[this.stuffing].price;
     for (let topping of this.toppings) {
-      totalPrice += topping.price;
+      totalPrice += Hamburger.toppings[topping].price;
     }
     return totalPrice;
   }
 
   calculateCalories() {
-    let totalCalories = this.size.calories + this.stuffing.calories;
+    let totalCalories =
+      Hamburger.sizes[this.size].calories +
+      Hamburger.stuffings[this.stuffing].calories;
     for (let topping of this.toppings) {
-      totalCalories += topping.calories;
+      totalCalories += Hamburger.toppings[topping].calories;
     }
     return totalCalories;
   }
 }
 
-const size = {
-  small: { price: 50, calories: 20 },
-  large: { price: 100, calories: 40 }
-};
-
-const stuffing = {
-  cheese: { price: 10, calories: 20 },
-  salad: { price: 20, calories: 5 },
-  potato: { price: 15, calories: 10 }
-};
-
-const topping = {
-  spice: { price: 15, calories: 0 },
-  mayo: { price: 20, calories: 5 }
-};
-
-const hamburger = new Hamburger(size.small, stuffing.cheese);
-hamburger.addTopping(topping.spice, topping.mayo);
+const hamburger = new Hamburger('small', 'cheese');
+hamburger.addTopping('spice', 'mayo');
+hamburger.addTopping('mayo');
 
 console.log(hamburger.calculateCalories());
 console.log(hamburger.calculatePrice());
